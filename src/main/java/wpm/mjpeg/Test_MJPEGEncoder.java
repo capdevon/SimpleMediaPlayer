fae55b7837fcb1354a28659925640600cb2a0a34
Wyatt Gillette <jefferydeaver2010@gmail.com>
2021-10-29 09:03:01 +0200

    correct typos

--- src/main/java/wpm/mjpeg/Test_MJPEGEncoder.java
+++ src/main/java/wpm/mjpeg/Test_MJPEGEncoder.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package wpm.mjpeg;
 
 import java.awt.image.BufferedImage;
@@ -32,14 +27,11 @@ public class Test_MJPEGEncoder {
         //Output movie dimension. In general it doesnt need to match input images dimension, 
         //but it is better to resize outside of java and input files which are already resized
         int outputWidth = 640;
-        int outputHeght = 360;
+        int outputHeight = 360;
         //Target fps. In general should match the input fps. Not really required though. 
         //This info is encoded into mjpeg but the playback is dependent on the value provided later on
         int fps = 30;
-        //Order number of first and last frame. All inclusive. 0 and 273 mean there is file prefix000.extName to prefix273extName
-        //Important. Convention assumes that the padding is consistent with the number of digits of endNumber
-        //It forces the file names to be as follows (with files from 0 to 273): prefix000.extName to prefix273extName.
-        //If the frames were for example 0 to 12 the files would be:  prefix00.extName to prefix12extName 
+        //Order number of first and last frame.
         int startNumber = 0;
         int endNumber = 273;
         //Folder containing frame files
@@ -49,38 +41,38 @@ public class Test_MJPEGEncoder {
         //Extension name
         String extName = "jpg";
         //GENERATE - watch console!
-        encodeImages(outputFile, outputWidth, outputHeght, fps, startNumber, endNumber, inputFolder, imagePrefix, extName);
+        encodeImages(outputFile, outputWidth, outputHeight, fps, startNumber, endNumber, inputFolder, imagePrefix, extName);
     }
 
     /**
      * Encodes separate jpgs files into mjpg file. See console for debugging.
      *
-     * @param outputFile  - Target file - full path with file e.g. C://Media//myFile.mjpg.
-     * @param outputWidth - Output movie dimension. In general it doesn't need to
-     *                    match input images dimension, but it is better to resize
-     *                    outside of java and input files which are already resized.
-     * @param outputHeght @see outputWidth 0
-     * @param fps         - Target fps. In general should match the input fps. Not
-     *                    really required though. This info is encoded into mjpeg
-     *                    but the playback is dependent on the value provided later on.
-     * @param startNumber - order number of first and last frame. All inclusive. 0
-     *                    and 273 mean there is file prefix000.extName to
-     *                    prefix273extName Important. Convention assumes that the
-     *                    padding is consistent with the number of digits of
-     *                    endNumber. It forces the file names to be as follows (with
-     *                    files from 0 to 273): prefix000.extName to
-     *                    prefix273extName. If the frames were for example 0 to 12
-     *                    the files would be: prefix00.extName to prefix12extName
-     * @param endNumber   @see startNumber
-     * @param inputFolder - Folder containing frame jpg files
-     * @param imagePrefix - Any prefix(before digits). Must be the same for every file
-     * @param extName     - Extension name
+     * @param outputFile   - Target file - full path with file e.g. C://Media//myFile.mjpg.
+     * @param outputWidth  - Output movie dimension. In general it doesn't need to
+     *                     match input images dimension, but it is better to resize
+     *                     outside of java and input files which are already resized.
+     * @param outputHeight @see outputWidth 0
+     * @param fps          - Target fps. In general should match the input fps. Not
+     *                     really required though. This info is encoded into mjpeg
+     *                     but the playback is dependent on the value provided later on.
+     * @param startNumber  - order number of first and last frame. All inclusive. 0
+     *                     and 273 mean there is file prefix000.extName to
+     *                     prefix273.extName Important. Convention assumes that the
+     *                     padding is consistent with the number of digits of
+     *                     endNumber. It forces the file names to be as follows
+     *                     (with files from 0 to 273): prefix000.extName to
+     *                     prefix273.extName. If the frames were for example 0 to 12
+     *                     the files would be: prefix00.extName to prefix12.extName
+     * @param endNumber    @see startNumber
+     * @param inputFolder  - Folder containing frame jpg files
+     * @param imagePrefix  - Any prefix(before digits). Must be the same for every file
+     * @param extName      - Extension name
      */
-    private static void encodeImages(File outputFile, int outputWidth, int outputHeght, int fps,
+    private static void encodeImages(File outputFile, int outputWidth, int outputHeight, int fps,
         int startNumber, int endNumber, String inputFolder, String imagePrefix, String extName) {
 
         System.out.println("Running " + inputFolder + imagePrefix + extName +
-            " with " + outputWidth + ":" + outputHeght +
+            " with " + outputWidth + ":" + outputHeight +
             " at " + fps +
             " frames from " + startNumber +
             " to " + endNumber +
@@ -88,7 +80,7 @@ public class Test_MJPEGEncoder {
 
         try {
             String filler = "00000000";
-            MJPEGGenerator mjpegGenerator = new MJPEGGenerator(outputFile, outputWidth, outputHeght, fps, endNumber - startNumber);
+            MJPEGGenerator mjpegGenerator = new MJPEGGenerator(outputFile, outputWidth, outputHeight, fps, endNumber - startNumber);
 
             for (int i = startNumber; i <= endNumber; i++) {
                 String counter = filler.substring(0, String.valueOf(endNumber).length() - String.valueOf(i).length()) + i;
